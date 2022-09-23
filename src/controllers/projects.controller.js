8f5b737181fc3ae8274855b88de308d0d3f432fd
Oscar Bernal <odbg2013@gmail.com>
2022-09-23 11:44:26 -0500

    update REST API con Sequelize

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,19 +1,26 @@
 import { Project } from '../models/Project.js'
 
-export const getProjects = (req, res) => {
-  res.send('getting projects')
+export const getProjects = async (req, res) => {
+  try {
+    const projects = await Project.findAll()
+    res.json(projects)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }
 
 export const createProject = async (req, res) => {
   const { name, priority, description } = req.body
 
-  const newProject = await Project.create({
-    name,
-    description,
-    priority,
-  })
+  try {
+    const newProject = await Project.create({
+      name,
+      description,
+      priority,
+    })
 
-  console.log(newProject)
-
-  res.send('creating projects')
+    res.json(newProject)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
 }

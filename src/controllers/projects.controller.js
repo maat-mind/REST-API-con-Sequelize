dab06967e7c10b0502ecc5ec07135f6c4277495f
Oscar Bernal <odbg2013@gmail.com>
2022-09-24 11:12:11 -0500

    update REST API con Sequelize

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -9,6 +9,22 @@ export const getProjects = async (req, res) => {
   }
 }
 
+export const getProject = async (req, res) => {
+  const { id } = req.params
+  try {
+    const project = await Project.findOne({
+      where: {
+        id,
+      },
+    })
+    if (!project) return res.status(404).json({ message: 'Project not found' })
+
+    res.json(project)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
 export const createProject = async (req, res) => {
   const { name, priority, description } = req.body
 
@@ -20,6 +36,41 @@ export const createProject = async (req, res) => {
     })
 
     res.json(newProject)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
+export const updateProject = async (req, res) => {
+  const { id } = req.params
+  const { name, priority, description } = req.body
+
+  try {
+    const project = await Project.findByPk(id)
+
+    project.name = name
+    project.priority = priority
+    project.description = description
+
+    await project.save()
+
+    res.json(project)
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
+export const deleteProject = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    await Project.destroy({
+      where: {
+        id,
+      },
+    })
+
+    res.sendStatus(204)
   } catch (error) {
     return res.status(500).json({ message: error.message })
   }
